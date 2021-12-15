9043b180f61ccd0dea394cb72308ead6f239769e
kabbya <kabbya.kantam@gmail.com>
2021-12-16 03:13:59 +0600

    Dependencies and Database Connection

---
+++ onlineMovieTicketBookingSystem/src/main/java/com/projectMovieTicket/OnlineMovieTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.projectMovieTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ onlineMovieTicketBookingSystem/src/test/java/com/projectMovieTicket/OnlineMovieTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectMovieTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMovieTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
